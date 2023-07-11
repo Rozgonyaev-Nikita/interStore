5292043d9c4e6009f44270b2d91a27656a710715
Nikita Rozgonyaev <karp.karp24karp@gmail.com>
2023-07-11 19:46:42 +0400

    приступил к валидации

--- src/components/AddTovar/AddTovar.tsx
+++ src/components/AddTovar/AddTovar.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { useAppDispatch } from "../../hooks/reduxHooks";
 import { addTovarsThunk, tovarsThunkPost } from "../../store/FetchTovars";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const initialTovar: ITovar = {
   id: 0,
@@ -18,6 +19,10 @@ const initialTovar: ITovar = {
     rate: 0,
     count: 0,
   },
+};
+type Inputs = {
+  example: string;
+  name: string;
 };
 
 const AddTovar = () => {
@@ -32,40 +37,73 @@ const AddTovar = () => {
     console.log("nwtv", newTovar);
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Inputs>({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    console.log("caz", JSON.stringify(onSubmit));
+  };
+
   return (
     <>
-      <Stack spacing={2} sx={{ width: "300px" }}>
-        <TextField
-          id="filled-basicf"
-          onChange={(e) => setTovar({ ...tovar, title: e.target.value })}
-          label="Заголовок"
-          variant="filled"
-        />
-        <TextField
-          id="filled-basicg"
-          onChange={(e) =>
-            setTovar({ ...tovar, price: Number(e.target.value) })
-          }
-          label="Цена"
-          variant="filled"
-        />
-        <TextField
-          id="filled-basicj"
-          onChange={(e) => setTovar({ ...tovar, description: e.target.value })}
-          label="Описание"
-          variant="filled"
-        />
-        <Grid container>
-          <Button
-            sx={{ width: "60%", margin: "0 auto" }}
-            variant="contained"
-            endIcon={<Add />}
-            onClick={addNewTovar}
-          >
-            Добавить
-          </Button>
-        </Grid>
-      </Stack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={2} sx={{ width: "300px" }}>
+          <TextField
+            id="name"
+            {...register("name", {
+              required: "Поле обязательное",
+              minLength: { value: 5, message: "Слишком коротко" },
+            })}
+            onChange={(e) => setTovar({ ...tovar, title: e.target.value })}
+            label="Заголовок"
+            variant="filled"
+            autoComplete="off"
+          />
+          {errors?.name ? (
+            <div style={{ margin: "0 0", padding: "0 0", color: "red" }}>
+              {errors?.name?.message || "karp"}
+            </div>
+          ) : (
+            <div
+              style={{ padding: "0 0 20px 0px", margin: "0 0", color: "red" }}
+            ></div>
+          )}
+          <TextField
+            id="filled-basicg"
+            onChange={(e) =>
+              setTovar({ ...tovar, price: Number(e.target.value) })
+            }
+            label="Цена"
+            variant="filled"
+            autoComplete="off"
+          />
+          <TextField
+            id="filled-basicj"
+            onChange={(e) =>
+              setTovar({ ...tovar, description: e.target.value })
+            }
+            label="Описание"
+            variant="filled"
+            autoComplete="off"
+          />
+          <Grid container>
+            <Button
+              sx={{ width: "60%", margin: "0 auto" }}
+              variant="contained"
+              endIcon={<Add />}
+              onClick={addNewTovar}
+              disabled={!isValid}
+            >
+              Добавить
+            </Button>
+          </Grid>
+        </Stack>
+      </form>
     </>
   );
 };
